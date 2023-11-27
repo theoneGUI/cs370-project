e52d087f3f7bdc990d975e1c3685f27b6bee98d4
unknown <49991562+theoneGUI@users.noreply.github.com>
2023-11-27 16:19:32 -0600

    Update presentation of Order report

--- OrderReturnOrderitemReport.php
+++ OrderReturnOrderitemReport.php
@@ -31,21 +31,11 @@ function output_error($title, $error) {
     <?php include_once 'bs.php'; ?>
 </head>
 <style>
-    .pizzaDataTable {
-        border-spacing: 0;
-    }
     .pizzaDataRow td {
         padding-left: 10px;
     }
     .pizzaDataHeader td {
         padding-right: 20px;
-    }
-
-    .pizzaDataDetailsCell {
-        padding-left: 20px;
-    }
-    .pizzaDataTable tr:nth-child(2n) {
-        background-color: #cccccc;
     }
 </style>
 <body>
@@ -58,7 +48,7 @@ if ($conError) {
 }
 else {
     function output_table_open() {
-        echo "<table class='table table-striped'>\n";
+        echo "<table class='table'>\n";
         echo "<thead>";
         echo "<tr class='pizzaDataHeader'>\n";
         echo "  <td>OrderID</td>\n";
@@ -76,7 +66,7 @@ else {
     }
 
     function output_order_row($OrderID, $UserName, $OrderDate, $CreditCardNumber, $OrderStatus, $TotalPrice) {
-        echo "<tr class='pizzaDataRow'>\n";
+        echo "<tr class='table-active'>\n";
         echo "  <td>{$OrderID}</td>\n";
         echo "  <td>{$UserName}</td>\n";
         echo "  <td>{$OrderDate}</td>\n";
@@ -87,26 +77,43 @@ else {
     }
 
     function output_order_details_row($items, $returns) {
-        $items_string = "None";
-        $return_str = "None";
-        if (count($items) != 0) {
-            $items_string = implode(", ", $items);
+        $output = "";
+        $needPrintItemHeader = true;
+        foreach ($items as $i) {
+            if ($i["Used"])
+                $output .= "<tr><td></td><td>{$i["Name"]}</td><td>".'$'."{$i["Price"]} (x{$i["Quantity"]})</td><td>{$i["Seller"]}</td></tr>";
         }
-        if (count($returns) != 0) {
-            $return_str = implode(", ", $returns);
+        if ($output == "") {
+            echo "<tr><td>No items ordered.</td></tr>";
         }
-        echo "<tr>";
-        echo "<td colspan='3' class='pizzaDataDetailsCell'>";
-        echo "Items ordered: {$items_string} <br>\n" .
-            " Items returned: {$return_str}<br>\n"
-            . "</td>";
-        echo "</tr>";
+        else {
+            $needPrintItemHeader = false;
+            echo "<tr><td>Items Ordered:<td></tr>";
+            echo "<tr><td></td><td>Item Name</td><td>Price (Quantity)</td><td>Seller Name</td></tr>";
+            echo $output;
+        }
+        $output = "";
+        foreach ($returns as $i) {
+            if ($i["Used"])
+                $output .= "<tr><td></td><td>{$i["Name"]}</td><td>".'$'."{$i["Price"]} (x{$i["Quantity"]})</td><td>{$i["Seller"]}</td></tr>";
+        }
+        if ($output == "") {
+            echo "<tr><td>No items returned.</td></tr>";
+        }
+        else {
+            echo "<tr><td>Items Returned:<td></tr>";
+            if ($needPrintItemHeader)
+                echo "<tr><td></td><td>Item Name</td><td>Price (Quantity)</td><td>Seller Name</td></tr>";
+            echo $output;
+        }
     }
 
 
     $query = "SELECT t0.OrderDate, t0.CreditCardNumber, t0.OrderID, t0.OrderStatus, t0.TotalPrice, 
                   t2.ItemID, t4.ItemName, t3.ItemID AS ReturnItemID, t5.ItemName AS ReturnItemName, t4.ItemName, CONCAT(t1.FirstName, ' ', t1.LastName) 
-                  AS UserName FROM `Order` t0 
+                  AS UserName, t6.ItemName as OrderItemName, t6.Price as OrderItemPrice, t2.ItemQuantity as OrderItemQuantity,
+                  t7.SellerName AS OrderItemSellerName, t8.SellerName AS ReturnItemSellerName, t3.ItemQuantity AS ReturnItemQuantity, t5.Price AS ReturnItemPrice
+                  FROM `Order` t0 
                   INNER JOIN User t1 
                   ON t0.UserID = t1.UserID
                   INNER JOIN OrderItem t2 
@@ -114,7 +121,10 @@ else {
                   LEFT OUTER JOIN `Return` t3 
                   ON t0.OrderID = t3.OrderID AND t2.ItemID = t3.ItemID
                   INNER JOIN Item t4 ON t2.ItemID =  t4.ItemID " .
-                    "LEFT OUTER JOIN Item t5 ON t3.ItemID = t5.ItemID";
+                    "LEFT OUTER JOIN Item t5 ON t3.ItemID = t5.ItemID ".
+                    "LEFT OUTER JOIN Item t6 ON t2.ItemID = t6.ItemID ".
+                    "LEFT OUTER JOIN Seller t7 ON t6.SellerID = t7.SellerID ".
+                    "LEFT OUTER JOIN Seller t8 ON t5.SellerID = t8.SellerID";
     $result = mysqli_query($con, $query);
     if ( ! $result) {
         if (mysqli_errno($con)) {
@@ -135,16 +145,28 @@ else {
                 if ($lastName != null) {
                     output_order_details_row($pizzas, $pizzerias);
                 }
-                output_order_row($row["OrderID"], $row["UserName"], $row["UserName"],
+                output_order_row($row["OrderID"], $row["UserName"],
                     $row["OrderDate"], $row["CreditCardNumber"], $row["OrderStatus"], $row["TotalPrice"]);
 
                 $pizzas = array();
                 $pizzerias = array();
             }
             if (!in_array($row["ItemName"], $pizzas))
-                $pizzas[] = $row["ItemName"];
+                $pizzas[] = array(
+                        "Used" => $row["ItemName"] != null,
+                        "Name" => $row["ItemName"],
+                        "Price" => $row["OrderItemPrice"],
+                        "Seller" => $row["OrderItemSellerName"],
+                        "Quantity" => $row["OrderItemQuantity"]
+                );
             if (!in_array($row["ReturnItemName"], $pizzerias))
-                $pizzerias[] = $row["ReturnItemName"];
+                $pizzerias[] = array(
+                        "Used" => $row["ReturnItemName"] != null,
+                        "Name" => $row["ReturnItemName"],
+                        "Price" => $row["ReturnItemPrice"],
+                        "Seller" => $row["ReturnItemSellerName"],
+                        "Quantity" => $row["ReturnItemQuantity"]
+                );
             $lastName = $row["OrderID"];
         }
         output_order_details_row($pizzas, $pizzerias);
