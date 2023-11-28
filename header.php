ddb78e258def77a9036e3742022953b196e0c50e
TomRush2001 <thomass.rush2020@gmail.com>
2023-11-28 17:09:59 -0600

    Implemented Style

--- header.php
+++ header.php
@@ -58,6 +58,22 @@
         <li><a href="#">Home</a></li>
         <li><a href="#">Shop</a></li>
         <li><a href="#">Contact</a></li>
+        <li class="nav-item"><a class="nav-link" href="/index.php">Amazon 2.0 Home</a></li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown"
+               href="#" role="button" aria-expanded="false">Data Import</a>
+            <ul class="dropdown-menu">
+                <li>
+                    <a class="dropdown-item" href="/OrderReturnOrderitemImport.php">Import Orders and Returns</a>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="/DepartmentSellerItemImport.php">Import Sellers and Items and Departments</a>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="/UserListListitemImport.php">Import User and Lists</a>
+                </li>
+            </ul>
+        </li>
     </ul>
 </nav>
 
@@ -85,38 +101,4 @@
 </body>
 </html>
 
-
-<ul class="nav">
-    <li class="nav-item"><a class="nav-link" href="/index.php">Amazon 2.0 Home</a></li>
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown"
-           href="#" role="button" aria-expanded="false">Data Import</a>
-        <ul class="dropdown-menu">
-            <li>
-                <a class="dropdown-item" href="/OrderReturnOrderitemImport.php">Import Orders and Returns</a>
-            </li>
-            <li>
-                <a class="dropdown-item" href="/DepartmentSellerItemImport.php">Import Sellers and Items and Departments</a>
-            </li>
-            <li>
-                <a class="dropdown-item" href="/UserListListitemImport.php">Import User and Lists</a>
-            </li>
-        </ul>
-    </li>
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown"
-           href="#" role="button" aria-expanded="false">Data Report</a>
-        <ul class="dropdown-menu">
-            <li>
-                <a class="dropdown-item" href="DepartmentSellerItemReport.php">Report on Departments, Items, and Sellers</a>
-            </li>
-            <li>
-                <a class="dropdown-item" href="UserListListitemReport.php">Report on Users and Lists</a>
-            </li>
-            <li>
-                <a class="dropdown-item" href="OrderReturnOrderitemReport.php">Report on Orders and Returns</a>
-            </li>
-        </ul>
-    </li>
-</ul>
 <div class="container">
