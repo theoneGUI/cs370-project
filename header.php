68e45bcb40d592a270a89a1d8870d6c345657c19
Taylor Sims <trsims@mail.bradley.edu>
2023-11-28 16:49:55 -0600

    Added Flair
    I made it look pretty.

--- header.php
+++ header.php
@@ -1,3 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Amazon 2.0</title>
+    <style>
+        body {
+            font-family: Baskerville, serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: #D2B48C;
+            color: #ffffff;
+            padding: 10px;
+            text-align: center;
+        }
+
+        nav {
+            background-color: #664229;
+            color: #ffffff;
+            padding: 10px;
+            text-align: center;
+        }
+
+        nav ul {
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+        }
+
+        nav ul li {
+            display: inline;
+            margin-right: 20px;
+        }
+
+        section {
+            padding: 20px;
+        }
+
+        .product {
+            border: 1px solid #ddd;
+            padding: 10px;
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+<body>
+
+<header>
+    <h1>Amazon 2.0</h1>
+</header>
+
+<nav>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Shop</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</nav>
+
+<section>
+    <div class="product">
+        <img src="product1.jpg" alt="Product 1">
+        <h2>Product 1</h2>
+        <p>Description of Product 1. </p>
+        <p>$19.99</p>
+        <button>Add to Cart</button>
+    </div>
+
+    <div class="product">
+        <img src="product2.jpg" alt="Product 2">
+        <h2>Product 2</h2>
+        <p>Description of Product 2. </p>
+        <p>$29.99</p>
+        <button>Add to Cart</button>
+    </div>
+
+    <!-- Add more product blocks as needed -->
+
+</section>
+
+</body>
+</html>
+
+
 <ul class="nav">
     <li class="nav-item"><a class="nav-link" href="/index.php">Amazon 2.0 Home</a></li>
     <li class="nav-item dropdown">
